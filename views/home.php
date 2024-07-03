73874deea5fe72e950c09fd1c25503a30ce9067b
FengXin <2085701453@qq.com>
2024-07-03 15:20:32 +0800

    third commit

---
+++ views/home.php
@@ -0,0 +1,25 @@
+<!-- <?php
+    include('../include/opening.inc.php');
+    $moviedbFilePath = '../moviedb/';
+    $directories = glob($moviedbFilePath . '*', GLOB_ONLYDIR);
+?> -->
+<?php
+    include('../include/opening.html');
+?>
+<!-- To complete -->
+<!DOCTYPE html>
+<html>
+    <body>
+        <link href="../css/home.css" type="text/css" rel="stylesheet">
+
+        <div id="content">
+	
+        </div>
+        <?php
+	        include('../include/closing.html');
+        ?>
+	</body>
+</html>
+
+
+
