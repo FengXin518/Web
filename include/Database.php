d0bf44848418c37aee3de360143b3c57b4ea57b4
FengXin <2085701453@qq.com>
2024-07-04 21:08:28 +0800

    seventh

---
+++ include/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+    ##### you MUST update those variables
+
+    $HOST     = 'localhost';
+    $DATABASE = 'doodle';
+    $USER     = 'root';
+    $PASSWORD = 'xing1211114';
+
+    ######################################################
+
+    $USER_TABLE   = 'user_info';
+    $VOTES_TABLE  = 'votes';
+    $VOTE_RECORDS_TABLE = 'vote_records';
+
+    $PDO = getPDO($HOST,$DATABASE,$USER,$PASSWORD);
+    
+    ######################################################
+
+    function getPDO($host,$db,$user,$pass) {
+        $charset = 'utf8';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        try {
+            return new PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+?>
---
+++ views/welcome_view.php
@@ -0,0 +1,39 @@
+<?php
+    include('../include/opening.php');
+    $moviedbFilePath = '../moviedb/';
+    $directories = glob($moviedbFilePath . '*', GLOB_ONLYDIR);
+?> 
+<!-- To complete -->
+<!DOCTYPE html>
+<html>
+    <body style="background-color:#e8efee;">
+        <link href="../css/welcome.css" type="text/css" rel="stylesheet">
+
+        <div id="content-1">
+            <label>Select your Doodle</label>
+        </div>
+        <div id="content-2">
+            <div>
+                <label class="title">Group Poll</label>
+                <label>Choose your times, let people vote and find the option that suit severyone.</label>
+                <button>Create</button>
+            </div>
+            <div>
+                <label class="title">1:1</label>
+                <label>Ask someone to pick a time from a list of specific times you selected.</label>
+                <button>Create</button>
+            </div>
+            <div>
+                <label class="title">Booking Page</label>
+                <label>Set your availability and create a link to let people book time with you.</label>
+                <button>Create</button>
+            </div>
+        </div>
+        <?php
+	        include('../include/closing.html');
+        ?>
+	</body>
+</html>
+
+
+
